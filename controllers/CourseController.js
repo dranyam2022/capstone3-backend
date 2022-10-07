d14d6834b0da9f691df6f33de6d0a964614367b4
earl.diaz <earl.diaz@zuitt.co>
2022-10-07 10:52:36 +0800

    Finished part 4

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -27,3 +27,51 @@ module.exports.addCourse = (data) => {
 		return value
 	})
 }
+
+module.exports.getAllCourses = () => {
+	return Course.find({}).then((result) => {
+		return result
+	})
+}
+
+module.exports.getAllActive = () => {
+	return Course.find({isActive: true}).then((result) => {
+		return result
+	})
+}
+
+module.exports.getCourse = (course_id) => {
+	return Course.findById(course_id).then((result) => {
+		return result 
+	})
+}
+
+module.exports.updateCourse = (course_id, new_data) => {
+	return Course.findByIdAndUpdate(course_id, {
+		name: new_data.name,
+		description: new_data.description, 
+		price: new_data.price 
+	}).then((updated_course, error) => {
+		if(error){
+			return false
+		}
+
+		return {
+			message: 'Course has been updated successfully!'
+		}
+	})
+}
+
+module.exports.archiveCourse = (course_id) => {
+	return Course.findByIdAndUpdate(course_id, {
+		isActive: false
+	}).then((archived_course, error) => {
+		if(error){
+			return false
+		}
+
+		return {
+			message: 'Course has been archived successfully!'
+		}
+	})
+}
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -15,4 +15,44 @@ router.post('/create', auth.verify, (request, response) => {
 	})
 })
 
+// Get all courses
+router.get('/', (request, response) => {
+	CourseController.getAllCourses().then((result) => {
+		response.send(result)
+	})
+})
+
+// Get all ACTIVE courses
+router.get('/active', (request, response) => {
+	CourseController.getAllActive().then((result) => {
+		response.send(result)
+	})
+})
+
+// Get single course
+router.get('/:courseId', (request, response) => {
+	CourseController.getCourse(request.params.courseId).then((result) => {
+		response.send(result)
+	})
+})
+
+// Update single course
+router.patch('/:courseId/update', auth.verify, (request, response) => {
+	CourseController.updateCourse(request.params.courseId, request.body).then((result) => {
+		response.send(result)
+	})
+})
+
+// MINI ACTIVITY (30 mins.)
+// Create a route and a controller function for setting the isActive field of a course to false
+// The user must be logged in to archive a course
+// The endpoint must be '/archive'
+// The response must be 'The course has been archived successfully!'
+// Send screenshot of response from postman to our hangouts GC
+router.patch('/:courseId/archive', auth.verify, (request, response) => {
+	CourseController.archiveCourse(request.params.courseId).then((result) => {
+		response.send(result)
+	})
+})
+
 module.exports = router 
